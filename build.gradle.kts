76b9c7d7f1535a0eb3703e4714689235d4cc3cd4
Andrea <andrea.severi12@studio.unibo.it>
2022-07-05 15:22:48 +0200

    add lombok to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,14 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    // AssertJ - main testing library
+    // AssertJ - https://assertj.github.io/doc/
     testImplementation("org.assertj:assertj-core:3.22.0")
+
+    // Lombok - https://projectlombok.org/
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    testCompileOnly("org.projectlombok:lombok:1.18.24")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
 }
 
 tasks.getByName<Test>("test") {
