9f277cdf156e339ddd2d8bc3bd0e6f4bd8e4c081
Andrea <andrea.severi12@studio.unibo.it>
2022-07-05 17:14:35 +0200

    add proper doc to int range
    Final note: check negative ranges and ranges with start value and final value as the same

--- src/main/java/range/integer/IntRange.java
+++ src/main/java/range/integer/IntRange.java
@@ -10,14 +10,15 @@ import java.util.stream.Stream;
  *
  * <p>Usage examples:
  *  <ul>
- *      <li>{@link IntRange#fromTo IntRange.fromTo(1, 5)} produces range of [1, 2, 3, 4, 5]</li>
- *      <li>{@link IntRange#fromTo IntRange.fromTo(5, 1)} produces range of [5, 4, 3, 2, 1]</li>
- *      <li>{@link IntRange#fromToWithStep IntRange.fromToWithStep(1, 10, 3)} produces range of [1, 4, 7, 10]</li>
- *      <li>{@link IntRange#evensFromTo IntRange.evensFromTo(1, 10)} produces range of [2, 4, 6, 8, 10]</li>
- *      <li>{@link IntRange#oddsFromTo  IntRange.oddsFromTo(1, 10)} produces range of [1, 3, 5, 7, 9]</li>
- *      <li>{@link IntRange#reverse() IntRange.fromTo(1, 5).reverse()} produces the same result as {@link IntRange#fromTo IntRange.fromTo(5, 1)}</li>
- *      <li>{@link IntRange#stream IntRange.fromTo(1, 5).stream()} produces a {@link Stream}{@code <Integer>} from IntRange [1, 2, 3, 4, 5]</li>
+ *      <li>IntRange.{@link #fromTo fromTo(1, 5)} produces range of [1, 2, 3, 4, 5]</li>
+ *      <li>IntRange.{@link #fromTo fromTo(5, 1)} produces range of [5, 4, 3, 2, 1]</li>
+ *      <li>IntRange.{@link #fromToWithStep fromToWithStep(1, 10, 3)} produces range of [1, 4, 7, 10]</li>
+ *      <li>IntRange.{@link #evensFromTo evensFromTo(1, 10)} produces range of [2, 4, 6, 8, 10]</li>
+ *      <li>IntRange.{@link #oddsFromTo  oddsFromTo(1, 10)} produces range of [1, 3, 5, 7, 9]</li>
+ *      <li>IntRange.fromTo(1, 5).{@link #reverse reverse()} produces the same result as IntRange.{@link #fromTo fromTo(5, 1)}</li>
+ *      <li>IntRange.fromTo(1, 5).{@link #stream stream()} produces a {@link Stream}{@code <Integer>} from IntRange [1, 2, 3, 4, 5]</li>
  *  </ul>
+ * </p>
  */
 @ToString
 @EqualsAndHashCode
@@ -32,6 +33,23 @@ public final class IntRange {
         this.step = step;
     }
 
+    /**
+     * Creates {@link IntRange} with {@link #from} as start value and {@link #to} as final value. Both are included in the range.
+     * <p>
+     * If {@link #from} > {@link #to}, a descending order range will be created.
+     * </p>
+     * <p>
+     * Usage examples:
+     *      <ul>
+     *          <li><pre>{@code IntRange.fromTo(1, 5)} produces range of [1, 2, 3, 4, 5]</li>
+     *          <li><pre>{@code IntRange.fromTo(5, 1)} produces range of [5, 4, 3, 2, 1]</li>
+     *      </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 1
+     */
     public static IntRange fromTo(int from, int to) {
         if (from > to) {
             return new IntRange(from, to, -1);
@@ -40,14 +58,56 @@ public final class IntRange {
         return new IntRange(from, to, 1);
     }
 
+    /**
+     * Creates {@link IntRange} with 0 as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.zeroTo(5)} produces range of [0, 1, 2, 3, 4, 5]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 1
+     */
     public static IntRange zeroTo(int to) {
         return new IntRange(0, to, 1);
     }
 
+    /**
+     * Creates {@link IntRange} with 1 as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.oneTo(5)} produces range of [1, 2, 3, 4, 5]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 1
+     */
     public static IntRange oneTo(int to) {
         return new IntRange(1, to, 1);
     }
 
+    /**
+     * Creates {@link IntRange} with {@link #from} as start value and {@link #to} as final value with {@link #step} as step value. Both are included in the range.
+     * <p>
+     * If {@link #from} > {@link #to}, a descending order range will be created.
+     * </p>
+     * <p>
+     * Usage examples:
+     *      <ul>
+     *          <li><pre>{@code IntRange.fromToWithStep(1, 15, 3)} produces range of [1, 4, 7, 10, 12, 15]</li>
+     *          <li><pre>{@code IntRange.fromToWithStep(10, 1, 3)} produces range of [10, 7, 4, 1]</li>
+     *      </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @param to final value of the range (included)
+     * @param step to skip values
+     * @return {@link IntRange} with step 1
+     */
     public static IntRange fromToWithStep(int from, int to, int step) {
         if (from > to) {
             return new IntRange(from, to, -step);
@@ -56,22 +116,85 @@ public final class IntRange {
         return new IntRange(from, to, step);
     }
 
+    /**
+     * Creates {@link IntRange} with 0 as start value and {@link #to} as final value with {@link #step} as step value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.zeroToWithStep(10, 4)} produces range of [0, 4, 8]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param to final value of the range (included)
+     * @param step to skip values
+     * @return {@link IntRange} with {@link #step} value
+     */
     public static IntRange zeroToWithStep(int to, int step) {
         return new IntRange(0, to, step);
     }
 
+    /**
+     * Creates {@link IntRange} with 1 as start value and {@link #to} as final value with {@link #step} as step value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.oneToWithStep(10, 4)} produces range of [1, 5, 9]</li>
+     *  </ul>
+     * </p>
+     * @param to final value of the range (included)
+     * @param step to skip values
+     * @return {@link IntRange} with {@link #step} value
+     */
     public static IntRange oneToWithStep(int to, int step) {
         return new IntRange(1, to, step);
     }
 
+    /**
+     * Creates {@link IntRange} with {@link #from} as start value and 1 as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.downToOne(5)} produces range of [5, 4, 3, 2, 1]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @return {@link IntRange} with step -1
+     */
     public static IntRange downToOne(int from) {
         return new IntRange(from, 1, -1);
     }
 
+    /**
+     * Creates {@link IntRange} with {@link #from} as start value and 0 as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.downToZero(5)} produces range of [5, 4, 3, 2, 1, 0]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @return {@link IntRange} with step -1
+     */
     public static IntRange downToZero(int from) {
         return new IntRange(from, 0, -1);
     }
 
+    /**
+     * Creates evens-only {@link IntRange} with {@link #from} as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.evensFromTo(1, 10)} produces range of [2, 4, 6, 8, 10]</li>
+     *      <li><pre>{@code IntRange.evensFromTo(10, 1)} produces range of [10, 8, 6, 4, 2]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 2 if to > from else -2
+     */
     public static IntRange evensFromTo(int from, int to) {
         if (from % 2 != 0) {
             if (from > to) {
@@ -84,10 +207,33 @@ public final class IntRange {
         return new IntRange(from, to, to > from ? 2 : -2);
     }
 
+    /**
+     * Creates evens-only {@link IntRange} with 0 as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.evensFromZeroTo(10)} produces range of [0, 2, 4, 6, 8, 10]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 2
+     */
     public static IntRange evensFromZeroTo(int to) {
         return new IntRange(0, to, 2);
     }
 
+    /**
+     * Creates evens-only {@link IntRange} with {@link #from} as start value and 0 as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.evensDownToZero(10)} produces range of [10, 8, 6, 4, 2, 0]</li>
+     *  </ul>
+     * </p>
+     *
+     * @return {@link IntRange} with step -2
+     */
     public static IntRange evensDownToZero(int from) {
         if (from % 2 != 0) {
             from++;
@@ -96,6 +242,20 @@ public final class IntRange {
         return new IntRange(from, 0, -2);
     }
 
+    /**
+     * Creates odds-only {@link IntRange} with {@link #from} as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.oddsFromTo(1, 10)} produces range of [1, 3, 5, 7, 9]</li>
+     *      <li><pre>{@code IntRange.oddsFromTo(10, 1)} produces range of [9, 7, 5, 3, 1]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param from start value of the range (included)
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 2 if to > from else -2
+     */
     public static IntRange oddsFromTo(int from, int to) {
         if (from % 2 == 0) {
             if (from > to) {
@@ -108,6 +268,33 @@ public final class IntRange {
         return new IntRange(from, to, to > from ? 2 : -2);
     }
 
+    /**
+     * Creates odds-only {@link IntRange} with 1 as start value and {@link #to} as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.oddsFromOneTo(10)} produces range of [1, 3, 5, 7, 9]</li>
+     *  </ul>
+     * </p>
+     *
+     * @param to final value of the range (included)
+     * @return {@link IntRange} with step 2
+     */
+    public static IntRange oddsFromOneTo(int to) {
+        return new IntRange(1, to, 2);
+    }
+
+    /**
+     * Creates odds-only {@link IntRange} with {@link #from} as start value and 1 as final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.oddsDownToOne(10)} produces range of [9, 7, 5, 3, 1]</li>
+     *  </ul>
+     * </p>
+     *
+     * @return {@link IntRange} with step -2
+     */
     public static IntRange oddsDownToOne(int from) {
         if (from % 2 == 0) {
             from++;
@@ -116,10 +303,15 @@ public final class IntRange {
         return new IntRange(from, 1, -2);
     }
 
-    public static IntRange oddsFromOneTo(int to) {
-        return new IntRange(1, to, 2);
-    }
-
+    /**
+     * Usage examples:
+     * <pre>
+     * {@code
+     *  Stream<Integer> oneToTenStream = IntRange.oneTo(10).stream();
+     * }
+     * </pre>
+     * @return {@link Stream}{@code <Integer>} with all values of the range
+     */
     public Stream<Integer> stream() {
         if (from > to) {
             return Stream.iterate(from, i -> i >= to, i -> i + step);
@@ -128,6 +320,17 @@ public final class IntRange {
         return Stream.iterate(from, i -> i <= to, i -> i + step);
     }
 
+    /**
+     * Reverses current IntRange, resulting in changing start value and final value.
+     * <p>
+     * Usage examples:
+     *  <ul>
+     *      <li><pre>{@code IntRange.from(1, 4).reverse()} produces range of [4, 3, 2, 1]</li>
+     *  </ul>
+     * </p>
+     *
+     * @return {@link IntRange} reversed
+     */
     public IntRange reverse() {
         return IntRange.fromToWithStep(to, from, -step);
     }
