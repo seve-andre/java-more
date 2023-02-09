e7a4e260be105a65faa85050804581983a91a07b
Andrea Severi <andrea.severi12@studio.unibo.it>
2023-02-09 15:15:37 +0100

    change to correct thrown exception

--- src/test/java/com/mitch/javamore/range/integer/IntRangeNegativeTest.java
+++ src/test/java/com/mitch/javamore/range/integer/IntRangeNegativeTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-class IntRangeNegativeTest {
+class RangeNegativeTest {
 
     @Test
     void testNegativeRange() {
-        IntRange negativeRange = IntRange.fromTo(-3, 1);
+        Range negativeRange = Range.fromTo(-3, 1);
 
         assertThat(negativeRange.stream())
                 .containsAll(IntStream.rangeClosed(-3, 1).boxed().toList());
@@ -19,9 +19,9 @@ class IntRangeNegativeTest {
 
     @Test
     void testNegativeExceptions() {
-        assertThatIllegalStateException().isThrownBy(() -> IntRange.downToOne(-2));
-        assertThatIllegalStateException().isThrownBy(() -> IntRange.downToZero(-2));
-        assertThatIllegalStateException().isThrownBy(() -> IntRange.evensDownToZero(-2));
-        assertThatIllegalStateException().isThrownBy(() -> IntRange.oddsDownToOne(-2));
+        assertThatIllegalArgumentException().isThrownBy(() -> Range.downToOne(-2));
+        assertThatIllegalArgumentException().isThrownBy(() -> Range.downToZero(-2));
+        assertThatIllegalArgumentException().isThrownBy(() -> Range.evensDownToZero(-2));
+        assertThatIllegalArgumentException().isThrownBy(() -> Range.oddsDownToOne(-2));
     }
 }
